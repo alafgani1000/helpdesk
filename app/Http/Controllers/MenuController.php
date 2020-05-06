4c59fdbe72f9292aa727e8a5f482af95c33ba388
alafgani1000 <irhabyaganal625@gmail.com>
2020-05-06 12:43:51 +0700

    add menu and stage controller

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,8 +3,99 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Menu;
+use App\Role;
+use Carbon\Carbon;
+use DataTables;
 
 class MenuController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('menus.index');
+    }
+
+    public function data()
+    {
+        $menus = DataTables(Menu::all())->toJson();
+        return $menus;
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kode' => 'required',
+            'label' => 'required',
+            'url' => 'required',
+            'name' => 'required'
+        ]);
+
+        $menu = Menu::create([
+            'kode' => $request->kode,
+            'label' => $request->label,
+            'url' => $request->url,
+            'name' => $request->name
+        ]);
+
+        return 'data created';
+    }
+
+    public function edit(Request $request)
+    {
+        $menu = Menu::find($request->id);
+        return view('menus.edit', compact('menu'));
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'kode' => 'required',
+            'label' => 'required',
+            'url' => 'required',
+            'name' => 'required'
+        ]);
+
+        $menu = Menu::where('id',$request->id)->update([
+            'kode' => $request->kode,
+            'label' => $request->label,
+            'url' => $request->url,
+            'name' => $request->name
+        ]);
+
+        return 'menu updated';
+    }
+
+    public function delete(Request $request)
+    {
+        $menu = Menu::find($request->id);
+        $menu->delete();
+
+        return 'success deleted';
+    }
+
+    public function assignRole(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'role_id' => 'required'
+        ]);
+
+        $menu = Menu::find($request->menu_id);
+        $menu->roles->attach($request->role_id);
+
+        return 'Assign success';
+    }
+
+    public function unAssignRole()
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'role_id' => 'required'
+        ]);
+
+        $menu = Menu::find($request->menu_id);
+        $menu->roles->detach($request->role_id);
+
+        return 'Assign success';
+    }
 }
--- app/Http/Controllers/StageController.php
+++ app/Http/Controllers/StageController.php
@@ -3,8 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Stage;
+use Carbon\Carbon;
+use DataTables;
 
 class StageController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('stages.index');
+    }
+
+    public function data()
+    {
+        return DataTables(Stage::all())->toJson();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'text' => 'required',
+            'description' => 'required'
+        ]);
+
+        $stage = Stage::create([
+            'text' => $request->text,
+            'description' => $request->description
+        ]);
+
+        return 'Success';
+    }
+
+    public function edit(Request $request)
+    {
+        $stage = Stage::find($request->id);
+        return view('stages.edit', compact('stage'));
+    }
+
+    public function update(Request $request)
+    {
+        
+        $request->validate([
+            'text' => 'required',
+            'description' => 'required'
+        ]);
+
+        $stage = Stage::where('id',$request->id)->update([
+            'text' => $request->text,
+            'description' => $request->description
+        ]);
+
+        return 'Success';
+    }
+
+    public function delete(Request $request)
+    {
+        $stage = Stage::find($request->id);
+        $stage->delete();
+
+        return 'Success';
+    }
 }
